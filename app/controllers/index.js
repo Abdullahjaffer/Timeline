3d67a384ec11055447d347ccec96be78b317e2ac
Abdullahjaffer <abdullah.jaffer@onebyte.biz>
2019-05-09 10:57:34 +0500

    corrected path to index.html

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,7 +1,10 @@
 var path = require('path');
 
 module.exports = function (router) {
-  router.get('/*', function(req, res) {
-    res.sendFile(path.join(__dirname, '../../', 'index.html'));
+  // router.get('*', function (req, res) {
+  //   res.sendFile("./index.html", { root: path.join(__dirname, './public') })
+  //   })
+  router.get('*', function(req, res) {
+    res.sendFile(path.join(__dirname, '../../', 'public/index.html'));
   });
 };
